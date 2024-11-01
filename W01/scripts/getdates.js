ea93230349d108f0c3ef69c6ec01c59b4b53f7eb
gabotii <93458917+gabotii@users.noreply.github.com>
2024-11-01 11:00:28 -0300

    home-page

---
+++ W01/scripts/getdates.js
@@ -0,0 +1,3 @@
+let d = new Date();
+document.getElementById("currentYear").innerHTML = `&copy; ${d.getFullYear()}`;
+document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
